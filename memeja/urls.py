2f990f7f9625f29ba52b4bd8e4d8b308af24b1a7
Delos Chang <lol.i.laugh@gmail.com>
2012-09-18 00:02:56 -0400

    First initial commit with basic css and front page

---
+++ memeja/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+
+from memejaapp.views import index
+from django.views.static import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'memeja.views.home', name='home'),
+    # url(r'^memeja/', include('memeja.foo.urls')),
+    url('^$', index),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
