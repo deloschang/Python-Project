7ca00e6bda298b802d22e2a319dbf4a3b29e1cc1
Delos Chang <lol.i.laugh@gmail.com>
2012-10-15 11:22:21 -0700

    quick commit

--- webapp/processor.py
+++ webapp/processor.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 
 def analytics(request):
     """
-    Returns analytics code.
+    Returns analytics code for Google Analytics
     """
     if not settings.DEBUG:
         return { 'analytics_code': render_to_string("analytics/analytics.html", { 'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY}) }
