8e798cb2e478291d3537f40321ec010df7268598
Delos Chang <lol.i.laugh@gmail.com>
2012-10-17 22:58:55 -0700

    prelim commit of userprofile extension

--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+
 class Experiences(models.Model):
     # user id
     title = models.CharField(max_length=60)
@@ -24,3 +26,17 @@ class Meme(models.Model):
 
     def __unicode__(self):
         return self.image.name
+
+class UserProfile(models.Model):  
+    user = models.OneToOneField(User)  
+    #other fields here
+    url_username = models.CharField(max_length=60)
+
+    def __str__(self):  
+          return "%s's profile" % self.user  
+
+def create_user_profile(sender, instance, created, **kwargs):  
+    if created:  
+       profile, created = UserProfile.objects.get_or_create(user=instance)  
+
+post_save.connect(create_user_profile, sender=User) 
