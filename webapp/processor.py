d5c4c0c2987d3f7af4af71020ce3b8ae8ea9e34e
Delos Chang <lol.i.laugh@gmail.com>
2012-10-23 13:36:01 -0700

    reordering to test

--- webapp/processor.py
+++ webapp/processor.py
@@ -1,6 +1,14 @@
 from django.conf import settings
 from django.template.loader import render_to_string
 
+def macromeme_key(request):
+    if not settings.DEBUG:
+        # production
+        return { 'flashappid': "320238601417173" }
+    else:
+        # development
+        return { 'flashappid': "479174175436255" }
+
 def analytics(request):
     """
     Returns analytics code for Google Analytics
@@ -11,10 +19,3 @@ def analytics(request):
     else:
         return { 'analytics_code': "" }
 
-def macromeme_key(request):
-    if not settings.DEBUG:
-        # production
-        return { 'flashappid': "320238601417173" }
-    else:
-        # development
-        return { 'flashappid': "479174175436255" }
