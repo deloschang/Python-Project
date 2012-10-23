fc8455df6011a092d57b6baf3cb26f54fe6d83ee
Delos Chang <lol.i.laugh@gmail.com>
2012-10-23 14:25:58 -0700

    added flashappid

--- webapp/processor.py
+++ webapp/processor.py
@@ -7,8 +7,8 @@ def analytics(request):
     """
     if not settings.DEBUG:
         # production
-        return { 'analytics_code': render_to_string("analytics/analytics.html", { 'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY}) }
+        return { 'analytics_code': render_to_string("analytics/analytics.html", { 'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY}), 'flashappid': '320238601417173' }
     else:
-        return { 'analytics_code': "" }
+        return { 'analytics_code': "", 'flashappid': '479174175436255'}
 
 
