b67ea56071d4ef39a9a2a690f635743b696af7cc
Delos Chang <lol.i.laugh@gmail.com>
2012-10-23 13:52:50 -0700

    moved to new file

--- webapp/processor.py
+++ webapp/processor.py
@@ -1,15 +1,6 @@
 from django.conf import settings
 from django.template.loader import render_to_string
 
-def macromeme_key(request):
-    raise Exception
-    if not settings.DEBUG:
-        # production
-        return { 'flashappid': "320238601417173" }
-    else:
-        # development
-        return { 'flashappid': "479174175436255" }
-
 def analytics(request):
     """
     Returns analytics code for Google Analytics
