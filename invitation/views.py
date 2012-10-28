3729476db7b2d7ed3dd19665c1d7da2304e6589d
Delos Chang <lol.i.laugh@gmail.com>
2012-10-27 20:36:45 -0700

    able to invite existing users through autocomplete. minor details needed to finish

--- invitation/views.py
+++ invitation/views.py
@@ -6,6 +6,7 @@ from django.template import RequestContext
 from django.shortcuts import render
 
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
@@ -27,12 +28,8 @@ def invite(request, success_url=None,
 
     # User entered in field and invited friend
     if request.method == 'POST':
-
-        ##### AUTOCOMPLETE, CONNECTED TO INVITE VIEW, NEED TO TRANSLATE USER OBJECT INTO DIFF FUNC #####
         #import pdb;
         #pdb.set_trace()
-
-        #if q in request.POST['q']:
 
         ### foreign key ==> request.POST['q']
         ### email ==> request.POST['q_text']
@@ -42,49 +39,54 @@ def invite(request, success_url=None,
         post_values = request.POST.copy()
         post_values['email'] = post_values['q_text'] # copy email from input into email for form
 
-        # no foreign key selected from autocomplete
-        if post_values['q'] == "":
-            messages.add_message(request, messages.INFO, 'no autocomplete')
-            return HttpResponseRedirect(success_url or reverse('webapp_index'))
+        # some autocomplete for existing users selected (e.g. |2|3|4|)
+        if post_values['q'] != "":
 
-        # foreign key selected from autocomplete
-        else: 
-            messages.add_message(request, messages.INFO, 'autocomplete')
-            return HttpResponseRedirect(success_url or reverse('webapp_index'))
-            
+            auto_list = post_values['q'].split('|')  # sep list of users
 
-        # for invitation processing
-        form = form_class(data=post_values, files=request.FILES)
-        if form.is_valid():
-            # Check for album number
-            if request.session['experience_no']:
-                invitation = InvitationKey.objects.create_invitation(request.user, form.cleaned_data["email"], request.session['experience_no'])
-                invitation.send_to(form.cleaned_data["email"], request.user, request.session['experience_no'])
+            linked_experience = request.session['experience_no']  # find selected album
+            for invited_existing_user_pk in auto_list[1:len(auto_list)-1]:  # slice off first | and last |
 
-                # success_url needs to be dynamically generated here; setting a
-                # a default value using reverse() will cause circular-import
-                # problems with the default URLConf for this application, which
-                # imports this file.
+                invited_existing_user = User.objects.get(pk=invited_existing_user_pk)
+                linked_experience.creator.add(invited_existing_user) 
 
+        # If an email value was entered
+        if post_values['email'] != "":
+            # for email invitation processing
+            form = form_class(data=post_values, files=request.FILES)
 
-                # Add a message that is output in templates/profile.html
-                messages.add_message(request, messages.INFO, 'Hooray! You invited your friend')
-                return HttpResponseRedirect(success_url or reverse('webapp_index'))
-    #if request.method == 'GET':
-        #return HttpResponseRedirect(success_url or reverse('webapp_index'))
+            # check if email is valid
+            if form.is_valid():
+                # Check for album number
+                if request.session['experience_no']:
+                    invitation = InvitationKey.objects.create_invitation(request.user, form.cleaned_data["email"], request.session['experience_no'])
+                    invitation.send_to(form.cleaned_data["email"], request.user, request.session['experience_no'])
+
+                    # success_url needs to be dynamically generated here; setting a
+                    # a default value using reverse() will cause circular-import
+                    # problems with the default URLConf for this application, which
+                    # imports this file.
+
+        # Add a message that is output in templates/profile.html
+        messages.add_message(request, messages.INFO, 'Hooray! You invited your friend')
+        return HttpResponseRedirect(success_url or reverse('webapp_index'))
+
     else:
+        # Show standard experience_display.html page
+
         form = form_class()
         request.session['experience_no'] = extra_context['experiences']
 
-    # add extra context to load memes and experiences in 
-    if extra_context is None:
-        extra_context = {}
-    context = RequestContext(request)
-    for key, value in extra_context.items():
-        context[key] = value
 
-    return render(request, template_name, {
-        'form': form,
-        'remaining_invitations': InvitationKey.objects.remaining_invitations_for_user(request.user),
-    }, context_instance=context)
+        # add extra context to load memes and experiences in 
+        if extra_context is None:
+            extra_context = {}
+        context = RequestContext(request)
+        for key, value in extra_context.items():
+            context[key] = value
+
+        return render(request, template_name, {
+            'form': form,
+            'remaining_invitations': InvitationKey.objects.remaining_invitations_for_user(request.user),
+        }, context_instance=context)
 invite = login_required(invite)
