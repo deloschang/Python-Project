ace700a24676984b9fdeefa267ed8d7dba74d568
Delos Chang <lol.i.laugh@gmail.com>
2012-11-01 20:28:07 -0700

    masonry fix and standardized js file

--- webapp/processor.py
+++ webapp/processor.py
@@ -5,11 +5,12 @@ def analytics(request):
     """
     1. Returns analytics code for Google Analytics
     2. Also returns flashapp id for macromeme generator
+    3. Returns standard js files used 
     """
     if not settings.DEBUG:
         # production
-        return { 'analytics_code': render_to_string("analytics/analytics.html", { 'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY}), 'flashappid': '320238601417173' }
+        return { 'analytics_code': render_to_string("processors/analytics.html", { 'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY}), 'flashappid': '320238601417173','standard_js': render_to_string("processors/standard_js.html", { 'STATIC_URL': settings.STATIC_URL})}
     else:
-        return { 'analytics_code': "", 'flashappid': '479174175436255'}
+        return { 'analytics_code': "", 'flashappid': '479174175436255', 'standard_js': render_to_string("processors/standard_js.html", { 'STATIC_URL': settings.STATIC_URL})}
 
 
