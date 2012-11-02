6acbdfd3cd9fbca0c7b5a9038cbbcb5a94e0eb2e
Delos Chang <lol.i.laugh@gmail.com>
2012-11-02 13:41:24 -0700

    merged with design fixes

--- webapp/views.py
+++ webapp/views.py
@@ -342,7 +342,9 @@ def helloworld(request):
             return HttpResponseRedirect(reverse('webapp_index'))
 
     else:
-        if request.user.get_profile().is_first_login:
+        ### bring back when done testing ###
+        #if request.user.get_profile().is_first_login:
+        if not request.user.get_profile().is_first_login:
             # not first time login ANYMORE
             request.user.get_profile().is_first_login = False
             request.user.get_profile().save()
@@ -351,6 +353,7 @@ def helloworld(request):
             school = request.user.get_profile().school 
             friend_form = TutorialNameForm()
             return render_to_response('user/tutorial.html', {'school':school, 'friend_form':friend_form}, RequestContext(request))
+        
 
 
 # Add new album for user   
