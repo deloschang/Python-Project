3b388d143c261f8ab8e7da3c2c28938be0546a17
Delos Chang <lol.i.laugh@gmail.com>
2012-11-20 11:23:27 -0800

    remove unique constraint of username

--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+User._meta.get_field('username')._unique = False
+
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
